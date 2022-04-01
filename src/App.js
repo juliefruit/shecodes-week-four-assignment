743e91d9f1e7c011aa866cb636af61e0421b5cbb
Julie <93963483+juliefruit@users.noreply.github.com>
2022-04-01 13:04:27 -0700

    Created Weather Search Engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchEngine from './SearchEngine';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchEngine />
+      <p><a href='https://github.com/juliefruit/shecodes-week-four-assignment.git' target='_blank'>Open-source code</a>, by Julie Wright</p>
     </div>
   );
 }
